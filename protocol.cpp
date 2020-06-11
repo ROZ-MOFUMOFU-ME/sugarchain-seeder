d0eb73cd011217c30c6d0894b7a9769c6be913cb
ROZ <mail@mofumofu.me>
2020-06-11 19:32:29 +0900

    susu

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0x9f, 0xeb, 0x4b, 0x9d };
+unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb9, 0xe9 };
 
 CMessageHeader::CMessageHeader()
 {
